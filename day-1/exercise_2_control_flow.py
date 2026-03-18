cb97e56f4188e8a3fa7531fd66fb6964e2da38a2
awaisahmadfg <awaisahmadfg@gmail.com>
2026-03-18 17:05:25 +0500

    Completed second exercise

---
+++ day-1/exercise_2_control_flow.py
@@ -0,0 +1,18 @@
+def grade_classifier(score):
+    if score >= 90:
+        return "Distinction"
+    elif score >= 60:
+        return "Pass"
+    else:
+        return "Fail"
+
+
+# Test
+test_scores = [0, 59, 60, 89, 90, 100]
+for s in test_scores:
+    print(s, "->", grade_classifier(s))
+
+print("\nClass results:")
+scores = [45, 72, 91, 60, 38, 85]
+for s in scores:
+    print(s, "->", grade_classifier(s))
